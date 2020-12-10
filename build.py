b60e51064b22c9ab3303fae17150d5aa6c9ac174
DriesDD <driesdedecker@gmail.com>
2020-12-10 16:51:34 +0100

    add

--- build.py
+++ build.py
@@ -1,8 +1,7 @@
-from flask import Flask, redirect, render_template, request
-import json
-import traceback
-import os
+from flask import Flask, redirect, Markup, render_template, request
+import waitress as waitress
 from waitress import serve
+import os
 
 app = Flask(__name__)
 
@@ -12,29 +11,49 @@ def home():
 
 @app.route('/<page>')
 def article(page):
-    pagedetails = next(i for i in PAGES if i[0] == page)
-    nextdetails = PAGES[(PAGES.index(pagedetails) -1) % pagecount]
-    prevdetails = PAGES[(PAGES.index(pagedetails) +1) % pagecount]
-    return render_template(pagedetails[1],
-    article = pagedetails[1], title=pagedetails[2], color=pagedetails[3], date=pagedetails[4],
-    nexttitle=nextdetails[2], nexturl=nextdetails[0],nextcolor=nextdetails[3],
-    prevtitle=prevdetails[2], prevurl=prevdetails[0],prevcolor=prevdetails[3])
+    if any(page in a for a in ARTICLES):
+        pagedetails = next(i for i in ARTICLES if i[0] == page)
+        nextdetails = ARTICLES[(ARTICLES.index(pagedetails) -1) % articlecount]
+        prevdetails = ARTICLES[(ARTICLES.index(pagedetails) +1) % articlecount]
+        return render_template(pagedetails[1],
+        article = pagedetails[1], title=pagedetails[2], color=pagedetails[3], date=pagedetails[4],
+        nexttitle=nextdetails[2], nexturl=nextdetails[0],nextcolor=nextdetails[3],
+        prevtitle=prevdetails[2], prevurl=prevdetails[0],prevcolor=prevdetails[3])
+    else:
+        if not any(page in a for a in OVERVIEWS):
+            page = '404'
+        pagedetails = next(i for i in OVERVIEWS if i[0] == page)
+        html = "<div class='flex'>"
+        for i in pagedetails[4]:
+            carddetails = next(j for j in ARTICLES if j[0] == i)
+            html += "<div class='flex-initial text-left font-display font-semibold py-2 px-4 text-"+carddetails[3]+"-200 bg-"+carddetails[3]+"-800 hover:bg-"+carddetails[3]+"-600 focus:bg-"+carddetails[3]+"-100 h-48 w-48 mr-4 mb-4'><a href=/"+carddetails[0]+"><span class='block text-right'>"+carddetails[4]+"</span><h3 class='font-display overflow-hidden font-semibold text-2xl'>"+carddetails[2]+"</h3></a></div>"
+        html += "</div>"
+        html = Markup(html)
+        return render_template(pagedetails[1],
+        article = pagedetails[1], title=pagedetails[2], color=pagedetails[3],cards=html)
 
-#every page is listed here as [URL resource name, HTML file path, title]
-PAGES = [
+#every article is listed here as [URL resource name, HTML file path, title, color,date]
+ARTICLES = [
     ['website','/articles/website.html','Making this website using Flask and Heroku','blue','Dec 8, 2020'],
-    ['reads','/articles/reads.html','Overview of reads','green','Dec 2, 2020'],
-    ['art','/articles/art.html','Art overview','pink','Dec 2, 2020'],
-    ['webdev','/articles/webdev.html','Web dev overview','blue','Dec 2, 2020'],
-    ['home','/articles/home.html','Welcome','green','Dec 2, 2020'],
-    ['about','/articles/about.html','Dries De Decker','gray','Dec 1, 2020'],
+    ['about','/articles/about.html','Dries De Decker','gray','Dec 1, 2020']
     ]
 
-pagecount = len(PAGES)
+articlecount = len(ARTICLES)
 
-if __name__ == "__main__":
-    app.debug = False
-    port = int(os.environ.get('PORT', 33507))
-    waitress.serve(app, port=port)
+#every overview is listed here as [URL resource name, HTML file path, title, color, [articles]]
+OVERVIEWS = [
+    ['404','/overviews/404.html','Page not found!','red',[]],
+    ['reads','/overviews/reads.html','Overview of reads','green',[]],
+    ['art','/overviews/art.html','Art overview','pink',[]],
+    ['webdev','/overviews/webdev.html','Web dev overview','blue',['website']],
+    ['home','/overviews/home.html','All articles','green',['website','about']]
+    ]
 
-    
+#this works after you do 'heroku config:set ON_HEROKU=1'
+if 'ON_HEROKU' in os.environ:
+    if __name__ == "__main__":
+        app.debug = False
+        port = int(os.environ.get('PORT', 33507))
+        waitress.serve(app, port=port)
+else:
+    app.run(debug=True)
