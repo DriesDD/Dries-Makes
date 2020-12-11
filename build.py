867bece77904d64810d787c0d0adc561297718a7
DriesDD <driesdedecker@gmail.com>
2020-12-11 20:22:34 +0100

    faster

--- build.py
+++ build.py
@@ -26,7 +26,7 @@ def article(page):
         html = "<div class='flex flex-row flex-wrap -mx-4'>"
         for i in pagedetails[4]:
             carddetails = next(j for j in ARTICLES if j[0] == i)
-            html += "<div class='flex-none shadow-sm text-left font-display font-semibold py-2 px-4 text-"+carddetails[3]+"-200 bg-"+carddetails[3]+"-800 hover:bg-"+carddetails[3]+"-600 focus:bg-"+carddetails[3]+"-100 h-48 w-48 mx-2 my-2 sm:mx-4 sm:my-4'><a href=/"+carddetails[0]+"><span class='block text-right'>"+carddetails[4]+"</span><h3 class='font-display overflow-hidden font-semibold h-full text-2xl'>"+carddetails[2]+"</h3></a></div>"
+            html += "<div class='flex-none shadow-sm text-left font-display py-2 px-4 text-"+carddetails[3]+"-200 bg-"+carddetails[3]+"-800 hover:bg-"+carddetails[3]+"-600 focus:bg-"+carddetails[3]+"-100 h-48 w-48 mx-2 my-2 sm:mx-4 sm:my-4'><a href=/"+carddetails[0]+"><span class='block text-right font-bold'>"+carddetails[4]+"</span><h3 class='font-display overflow-hidden h-full text-2xl'>"+carddetails[2]+"</h3></a></div>"
         html += "</div>"
         html = Markup(html)
         return render_template(pagedetails[1],
