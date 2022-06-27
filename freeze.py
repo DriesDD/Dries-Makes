368e578242a8626ab3ca4e0c8c5948e0ff382f0f
DriesDD <driesdedecker@gmail.com>
2022-06-27 22:23:19 +0200

    better formatting

--- freeze.py
+++ freeze.py
@@ -8,9 +8,9 @@ app.config['FREEZER_DESTINATION'] = 'DriesDD.github.io'
 @freezer.register_generator
 def page_url_generator():
     for article in ARTICLES:
-        yield '/'+article[0]
+        yield '/'+article[0]+'.html'
     for overview in OVERVIEWS:
-        yield '/'+overview[0]
+        yield '/'+overview[0]+'.html'
 
 if __name__ == '__main__':
     freezer.freeze()
