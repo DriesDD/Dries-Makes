eed12feec15e36cf66fb539bf2b53e8a2d08e95c
DriesDD <driesdedecker@gmail.com>
2022-06-28 00:12:47 +0200

    add .html to filenames to get around github pages issue

--- freeze.py
+++ freeze.py
@@ -1,5 +1,6 @@
 from flask_frozen import Freezer
 from build import app, ARTICLES, OVERVIEWS
+import os
 
 freezer = Freezer(app)
 
@@ -8,10 +9,15 @@ app.config['FREEZER_DESTINATION'] = 'DriesDD.github.io'
 @freezer.register_generator
 def page_url_generator():
     for article in ARTICLES:
-        yield '/'+article[0]+'.html'
+        yield '/'+article[0]
     for overview in OVERVIEWS:
-        yield '/'+overview[0]+'.html'
+        yield '/'+overview[0]
 
 if __name__ == '__main__':
     freezer.freeze()
     freezer.run(debug=True)
+
+for article in ARTICLES:        
+    os.rename('DriesDD.github.io/'+article[0],'DriesDD.github.io/'+article[0]+'.html')
+for overview in OVERVIEWS:        
+    os.rename('DriesDD.github.io/'+overview[0],'DriesDD.github.io/'+overview[0]+'.html')
